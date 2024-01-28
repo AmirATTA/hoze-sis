0a40fc7a65a1291aca694e9f68f5cd2a3a386b67
AmirATTA <amirmohammad.sh8326@gmail.com>
2024-01-29 02:32:01 +0330

    Fix typo

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ use App\Http\Controllers\api\AnnouncementController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will.
+| routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "api" middleware group. Make something great!
 |
 */
